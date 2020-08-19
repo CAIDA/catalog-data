6f8b002c84c368400026f6d89c5eadd9e40c27a5
louis910 <louis19930910@gmail.com>
2020-08-19 14:54:14 -0700

    Update parse_peeringdb.py

--- sources/recipe/how_to_parse_dataset_peeringdb/parse_peeringdb.py
+++ sources/recipe/how_to_parse_dataset_peeringdb/parse_peeringdb.py
@@ -5,58 +5,70 @@ import json
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", dest='dataset', type=str, default="peeringdb_dump_2016_01_01.sql", help="")
-    parser.add_argument("-j", dest='json_dataset', type=str, default= "peeringdb_2_dump_2020_01_01.json")
+    parser.add_argument("-j", dest='json_dataset', default = None)
+    parser.add_argument('-sq', dest='sqlite_dataset', default=None)
+    parser.add_argument('-get', dest='type', type=str, help='Input what kind of object you would like to get.')
+    parser.add_argument('-id', dest='single_id', type=int)
     args = parser.parse_args()
 
+    if args.json_dataset and args.type and not args.single_id:
+        print(get_object(args.json_dataset, args.type))
+    elif args.json_dataset and args.type and args.single_id:
+        print(get_single_object(args.json_dataset, args.type, args.single_id))
 
-    with open(args.json_dataset) as f:
+    elif args.sqlite_dataset and args.type and not args.single_id:
+        print(get_sqlite_object(args.sqlite_dataset, args.type)) 
+
+    elif args.sqlite_dataset and args.type and args.single_id:
+        print(get_sqlite_single_object(args.sqlite_dataset, args.type, args.single_id)) 
+
+def get_json_data(filename):
+    with open(filename) as f:
         data = json.load(f)
+    return data
 
-    print(data.keys())
-    # print(data['ix']['data'][0])
-    # print("\n\n")
-    # print(data['ix']['data'][1])
-    # print(len(data['ix']['data']))
-    # print(len(data['ix']))
+def get_object(filename, type):
+    data = get_json_data(filename)
+    return data[type]['data']
 
-    # print("Initial sqlit3")
-    # connection = sqlite3.connect(":memory:") 
-    # cursor = connection.cursor()
+def get_single_object(filename, type, target_id):
+    data = get_json_data(filename)
+    for item in data[type]['data']:
+        if item['id'] == target_id:
+            return item
 
-    # sql_file = open(args.dataset)
-    # read_line = sql_file.read()
-    # cursor.executescript(read_line)
+def get_sqlite_object(filename, type):
+    connection = sqlite3.connect(filename)
+    cursor = connection.cursor()
 
-    # for r in cursor.execute("SELECT * FROM mgmtFacilities"):
-    #     print(r)
-def get_fac():
-    pass
-def get_single_fac():
-    pass
-def get_ix():
-    pass
-def get_single_ix():
-    pass
-def get_ixfac():
-    pass
-def get_single_ixfac():
-    pass
-def get_ixlan():
-    pass
-def get_ixpfx():
-    pass
-def get_net():
-    pass
-def get_single_net():
-    pass
-def get_org():
-    pass
-def get_single_org():
-    pass
-def 
+    # The result of a "cursor.execute" can be iterated over by row
+    data = cursor.execute('SELECT * FROM ' + type + ';')
+    names = [description[0] for description in cursor.description]
+    obj = []
+    for row in data:
+        single_obj = {}
+        for i in range(len(names)):
+            single_obj[names[i]] = row[i]
+        obj.append(single_obj)
 
+    # Be sure to close the connection
+    connection.close()
+    return obj
 
-'ix', 'ixfac', 'net', 'ixlan', 'as_set', 'api', , 'netfac', 'org', 'ixpfx', 'netixlan', 'poc'])
+def get_sqlite_single_object(filename, type, target_id):
+    connection = sqlite3.connect(filename)
+    cursor = connection.cursor()
+
+    # The result of a "cursor.execute" can be iterated over by row
+    data = cursor.execute('SELECT * FROM ' + type + ';')
+    names = [description[0] for description in cursor.description]
+    for row in data:
+        if row[0] == target_id:
+            single_obj = {}
+            for i in range(len(names)):
+                single_obj[names[i]] = row[i]
+            connection.close()
+            return single_obj
+
 if __name__ == '__main__':
     main()
