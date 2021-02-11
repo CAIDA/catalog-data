d94207b333c27171c551468bf2a1ea52de3e8216
DonaldWolfson <dwolfson486@gmail.com>
2021-02-11 11:52:20 -0800

    issue #109 Created initial script

---
+++ scripts/caida_dataset_conversion.py
@@ -0,0 +1,161 @@
+#!  /usr/bin/env python3
+__author__ = "Donald Wolfson"
+__email__ = "dwolfson@zeus.caida.org"
+# This software is Copyright (C) 2020 The Regents of the University of
+# California. All Rights Reserved. Permission to copy, modify, and
+# distribute this software and its documentation for educational, research
+# and non-profit purposes, without fee, and without a written agreement is
+# hereby granted, provided that the above copyright notice, this paragraph
+# and the following three paragraphs appear in all copies. Permission to
+# make commercial use of this software may be obtained by contacting:
+#
+# Office of Innovation and Commercialization
+#
+# 9500 Gilman Drive, Mail Code 0910
+#
+# University of California
+#
+# La Jolla, CA 92093-0910
+#
+# (858) 534-5815
+#
+# invent@ucsd.edu
+#
+# This software program and documentation are copyrighted by The Regents of
+# the University of California. The software program and documentation are
+# supplied “as is”, without any accompanying services from The Regents. The
+# Regents does not warrant that the operation of the program will be
+# uninterrupted or error-free. The end-user understands that the program
+# was developed for research purposes and is advised not to rely
+# exclusively on the program for any reason.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+# DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+# INCLUDING LOST PR OFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+# DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY
+# DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+# SOFTWARE PROVIDED HEREUNDER IS ON AN “AS IS” BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+
+################################## Imports #####################################
+
+import argparse
+import json
+import sys
+import re
+import os
+
+############################## Global Variables ################################
+
+# Datasets:
+id_2_object = {}
+seen_datasets = set()
+seen_softwares = set()
+
+# Definitions:
+re_json = re.compile(r".json")
+re_mkdn = re.compile(r".md")
+re_mdta = re.compile(r"~~~metadata")
+re_dlim = re.compile(r"~~~")
+
+# File Paths:
+path = None
+
+################################# Main Method ##################################
+
+def main(argv):
+    global path
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", type=str, default=None, dest="path", help="Path to catalog-data-caida/sources")
+    args = parser.parse_args()
+
+    # Edge Case: Exit if no path given.
+    if args.path == None:
+        return
+    else:
+        path = args.path
+
+    update_seen_datasets()
+    update_seen_softwares()
+    parse_catalog_data_caida()
+
+############################### Helper Methods #################################
+
+# Map all known dataset ID's to seen_datasets.
+def update_seen_datasets():
+    global seen_datasets
+
+    # Iterate over each dataset JSON and keep track of their IDs.
+    for file in os.listdir("sources/dataset"):
+        # Edge Case: Skip if file is not a .json file.
+        if not re_json.search(file):
+            continue
+        
+        seen_datasets.add(file[:file.index(".")])
+
+
+# Map all known software ID's to seen_softwares.
+def update_seen_softwares():
+    global seen_softwares
+
+    # Iterate over each software JSON and keep track of their IDs.
+    for file in os.listdir("sources/software"):
+        # Edge Case: Skip if file is not a .json file.
+        if not re_json.search(file):
+            continue
+        
+        seen_softwares.add(file[:file.index(".")])
+
+
+# Parse of all .md objects in catalog-data-caida/sources.
+def parse_catalog_data_caida():
+    global id_2_object
+    global seen_datasets
+    global seen_softwares
+    global re_mkdn
+    global re_mdta
+    global re_dlim
+    global path
+
+    # Iterate over each file in catalog-data-caida/sources.
+    for file in os.listdir(path):
+        # Edge Case: Skip if file is not a .md file.
+        if not re_mkdn.search(file):
+            continue
+
+        file_name = file[:file.index(".")]
+
+        # Edge Case: Skip files that have already been seen.
+        if file_name in seen_datasets or file_name in seen_softwares:
+            continue     
+
+        # Iterate over file and grab all the metadata.
+        with open(file, "r") as curr_file:
+            found_metadata = False
+            curr_metadata = ""
+            curr_line = curr_file.readline()
+            while curr_line:
+                # Base Case: Start parsing metadata once starting delim found.
+                if re_mdta.search(curr_line):
+                    found_metadata = True
+
+                # Base Case: End parsing curr_file once ending delim found.
+                if re_dlim.search(curr_line):
+                    break
+
+                # Parse all data within the metadata block.
+                if found_metadata:
+                    curr_metadata += curr_line.strip()
+
+                curr_line = curr_file.readline()
+        
+        # Parse the curr_metadata for JSON object.
+        print(curr_metadata)
+        return
+
+# Run the script given the inputs from the terminal.
+main(sys.argv[1:])
