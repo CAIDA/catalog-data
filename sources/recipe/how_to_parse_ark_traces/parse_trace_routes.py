7cb07d09da2980bd2756723090181fd87b998561
Sasha <zabegalinsasha@gmail.com>
2022-02-10 15:22:25 -0800

    added: simple parse script

--- sources/recipe/how_to_parse_ark_traces/parse_trace_routes.py
+++ sources/recipe/how_to_parse_ark_traces/parse_trace_routes.py
@@ -4,34 +4,37 @@ import socket
 
 def simple_ip_path(ark_trace):
     
-    simple_trace_path_text = '-> traceroute to {} ({}), {} hops max\n'
-    simple_hop_path_text = '\t{}) Hop Address: {} ({}) | RTT: {:.4} ms | TTL: {} ms'
+    simple_trace_path_text = '-> traceroute to {} ({}), {} hops max, {} byte packets\n'
+    simple_hop_path_text = '\t{}) Hop Address: {} ({}) | RTT: {:.3f} ms'
     
     trace_dst_ip = ark_trace['dst']
     trace_dst_name = socket.getfqdn(ark_trace['dst'])
     trace_hop_count = ark_trace['hop_count']
+    trace_byte_packets = ark_trace['probe_count']
     
     path_counter = 1
     
-    print(simple_trace_path_text.format(trace_dst_name, trace_dst_ip, trace_hop_count))
+    print(simple_trace_path_text.format(trace_dst_name, trace_dst_ip, 
+                                        trace_hop_count, trace_byte_packets))
     print('{')
     
-    for hop in ark_trace['hops']:
+    hops = ark_trace['hops']
+    
+    for hop in hops:
         
         hop_addr_ip = hop['addr']
         hop_addr_name = socket.getfqdn(hop['addr'])
           
         hop_rtt = hop['rtt']
-        hop_ttl = hop['reply_ttl']
         
-        
-        print(simple_hop_path_text.format(path_counter, hop_addr_name, hop_addr_ip, hop_rtt, hop_ttl))
+        print(simple_hop_path_text.format(path_counter, hop_addr_name, hop_addr_ip, hop_rtt))
         path_counter += 1
     
     print('}')
 
 
 def read_warts_file():
+    
     # read one line at a time from std in
     warts_json_file = sys.stdin
     # ignore first line
@@ -44,6 +47,7 @@ def read_warts_file():
         simple_ip_path(ark_trace)
         
 def main():
+    
     read_warts_file()
 
 if __name__ == "__main__":
