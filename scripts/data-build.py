8ec7f001982ca5accc3b27a241c01dc083e60469
bhuffaker <bhuffake@gmail.com>
2022-04-12 12:06:33 -0700

    md -> markdown

--- scripts/data-build.py
+++ scripts/data-build.py
@@ -899,7 +899,7 @@ def recipe_process(path):
                                 else:
                                     tab_content += line
                             if extention == "md":
-                                f = "md"
+                                f = "markdown"
                             else:
                                 f = "text"
                             tabs.append({
