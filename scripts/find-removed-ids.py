14568557553597089362d66ac8855a169f0fcb14
Victor Ren <victorren2002@gmail.com>
2022-08-30 12:19:26 -0700

    added find-removed-ids.py with comments

--- scripts/find-removed-ids.py
+++ scripts/find-removed-ids.py
@@ -1,8 +1,55 @@
 #!/usr/bin/env python3
+__author__ = "Bradley Huffaker", "Victor Ren"
+__email__ = "<bradley@caida.org>", "<vren@ucsd.edu>"
+# This software is Copyright (C) 2022 The Regents of the University of
+# California. All Rights Reserved. Permission to copy, modify, and
+# distribute this software and its documentation for educational, research
+# and non-profit purposes, without fee, and without a written agreement is
+# hereby granted, provided that the above copyright notice, this paragraph
+# and the following three paragraphs appear in all copies. Permission to
+# make commercial use of this software may be obtained by contacting:
+#
+# Office of Innovation and Commercialization
+#
+# 9500 Gilman Drive, Mail Code 0910
+#
+# University of California
+#
+# La Jolla, CA 92093-0910
+#
+# (858) 534-5815
+#
+# invent@ucsd.edu
+#
+# This software program and documentation are copyrighted by The Regents of
+# the University of California. The software program and documentation are
+# supplied "as is", without any accompanying services from The Regents. The
+# Regents does not warrant that the operation of the program will be
+# uninterrupted or error-free. The end-user understands that the program
+# was developed for research purposes and is advised not to rely
+# exclusively on the program for any reason.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+# DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+# INCLUDING LOST PR OFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+# DOCUMENTATION, EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY
+# DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+# SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+#
+
+# USAGE
+# ./find-removed-ids.py > <output file>
+# We recommend outputting to a file for efficiency
+# If file not specified then program will print to stdout
 
 # imports
 import requests
 
+# Set to store each unique id
 ids_current = set()
 query = """{
   search {
@@ -13,12 +60,16 @@ query = """{
     }
   }
 }"""
+
+# Request from CAIDA catalog api
 request = requests.post("https://api.catalog.caida.org/", json={'query': query})
 if request.status_code == 200:
     response = request.json()
 
+# Iterate over all nodes from response
 for nodes in response["data"]["search"]["edges"]:
-    ids_current.add(nodes["node"]["id"]);
+    ids_current.add(nodes["node"]["id"])
 
+# Outputs ID
 for id in ids_current:
     print(id)
