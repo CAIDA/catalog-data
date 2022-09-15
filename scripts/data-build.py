5351a9202b8760798096a42ae3b3c9294b571695
bhuffaker <bhuffake@gmail.com>
2022-09-15 11:18:21 -0700

    move venue.obj to venu.name into obj_finish

--- scripts/data-build.py
+++ scripts/data-build.py
@@ -555,16 +555,11 @@ def object_date_add(obj):
     #        else:
     #            utils.error_add(obj["filename"], "missing dateCreated and dateModified, please add dateCreated or dateModified")
     
-    if obj["__typename"] == "Media" and "presenters" in obj:
+    if "presenters" in obj:
+        print (obj["id"])
         for person_venue in obj["presenters"]:
             if "date" in person_venue:
                 obj["date"] = utils.date_parse(person_venue["date"])
-            if "venue" in person_venue and "venue" == person_venue["venue"][:5]:
-                vid = person_venue["venue"]
-                if vid in id_object:
-                    person_venue["venue"] = id_object[vid]["name"]
-                else:
-                    utils.error_add(obj["filename"], f'missing venue: {person_venue["venue"]}')
         if "date" not in obj:
             if "deprecated" not in obj:
                 utils.error_add(obj["filename"], "missing date, please add date")
@@ -750,6 +745,15 @@ def object_finish(obj):
                                     person_org[k] = person["id"]
                                 else:
                                     error = True
+
+                        # For now we are not suppporting venues as objects, so replace with just # string name
+                        if "venue" in person_org and "venue" == person_org["venue"][:5]:
+                            vid = person_org["venue"]
+                            if vid in id_object:
+                                person_org["venue"] = id_object[vid]["name"]
+                            else:
+                                utils.error_add(obj["filename"], f'missing venue: {person_venue["venue"]}')
+                                del person_org["venue"]
                     # if the people are in a list of person ids
                     elif type(person_org) == str and person_org[:7] == "person:":
                         person = person_lookup_id(obj["filename"], person_org)
