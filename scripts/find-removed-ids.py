1e0120cf467dd80f565f0c8e0a04938b44863c89
Victor Ren <victorren2002@gmail.com>
2023-08-24 14:48:29 +0800

    Finished getting ids of all deleted objects

--- scripts/find-removed-ids.py
+++ scripts/find-removed-ids.py
@@ -1,5 +1,7 @@
 import subprocess
+import requests
 
+# Find all the deleted files in the git repo
 def find_deleted_files(repo_path):
     # Git log
     git_log_raw = subprocess.run(
@@ -17,23 +19,64 @@ def find_deleted_files(repo_path):
 
     git_log_delete_list = git_log_delete.split('\n')
     invalid_entries = []
+    deleted_files = []
     for entry in git_log_delete_list:
         try:
             file_path = entry.split(' ')[4].split('/')
             # File path should have 3 entries
             if len(file_path) == 3:
-                pass
+                if file_path[2].endswith(".json") or file_path[2].endswith(".md") or file_path[2].endswith(".MD"):
+                    # Ignore all person objects
+                    if file_path[1] != "person": 
+                        deleted_files.append(file_path)
             else:
                 invalid_entries.append(entry)
             
         except:
             invalid_entries.append(entry)
-    deleted_files = []
+    
+    return deleted_files
 
 
+# Convert file to id (e.g. software:dzdb_api)
+def convert_file_to_id(file):
+    return f"{file[1]}:{file[2].replace('.json', '').replace('.md', '').replace('.MD', '')}"
 
-def convert_files_to_id(file_list):
-    for file in file_list:
-        print(file.split(' ')[3])
+# Find all deleted files and return a set of all their ids
+def find_deleted_files_id():
+    deleted_files_id = set()
+    deleted_files = find_deleted_files('.')
+    for f in deleted_files:
+        deleted_files_id.add(convert_file_to_id(f))
+    return deleted_files_id
 
-find_deleted_files(".")
+
+def get_current_ids(catalog_api_url):
+    ids_current = set()
+    query = """{
+      search {
+        edges {
+           node {
+              id   
+           }
+        }
+      }
+    }"""
+    
+    request = requests.post(catalog_api_url, json={'query': query})
+    if request.status_code == 200:
+        response = request.json()
+        for edge in response["data"]["search"]["edges"]:
+            ids_current.add(edge["node"]["id"])
+    
+    return ids_current
+
+def main():
+    # Get ids of all deleted files
+    deleted_files_by_id = find_deleted_files_id()
+    
+    # Need to query CAIDA API
+    
+
+
+main()
